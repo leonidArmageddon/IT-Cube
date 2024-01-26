3b48e7e5a0b1c35f6871ccec04d28b87fd52817f
leonidArmageddon <157305821+leonidArmageddon@users.noreply.github.com>
2024-01-26 13:35:28 +0800

    Update task6.py

--- 4. условия/task6.py
+++ 4. условия/task6.py
@@ -2,10 +2,10 @@ x1, y1, x2, y2 = map(int, input().split())
 def move(x1, y1, x2, y2):
     if 1 <= x1 <= 8 and 1 <= y1 <= 8 and 1 <= x2 <= 8 and 1 <= y2 <= 8:
         if x1 == x2 or y1 == y2:
-            print("Да")
+            return("Да")
         else:
-            print("Нет")
+            return("Нет")
     else:
-        print("Координаты выходят за пределы доски")
+        return("Координаты выходят за пределы доски")
 
 print(move(x1, y1, x2, y2))
