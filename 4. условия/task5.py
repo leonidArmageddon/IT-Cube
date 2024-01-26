5ea1ce39cdfb68a798fe3ebe19371e0b62930755
leonidArmageddon <157305821+leonidArmageddon@users.noreply.github.com>
2024-01-26 13:35:03 +0800

    Update task5.py

--- 4. условия/task5.py
+++ 4. условия/task5.py
@@ -1,7 +1,6 @@
 # ЗАДАНИЕ 5
-
-a = int(input('Введите число '))
-if a > 999 and a % 7 == 0 or a % 17 == 0:
-    print('Красивое!')
+number = int(input("Введите число: "))
+if len(str(number)) == 4 and (number % 7 == 0 or number % 17 == 0):
+    print("Красивое")
 else:
-    print('Страшное!')
+    print("Страшное")
